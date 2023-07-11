26619c01812e849805cf0447f45ce8c58a744d75
Gustavo Jesinski <gustavojesinskib@gmail.com>
2023-07-11 08:51:34 -0300

    added deploy script; commented test sales reps users.

--- getSalesRepresentatives.ts
+++ getSalesRepresentatives.ts
@@ -1,15 +1,27 @@
 import { SalesRepresentative } from "./types";
 
-const gustavo = {
-  email: "gustavo.j@dotconceito.com",
-  name: "Gustavo Jesinski",
-  phone: "+5551998606868",
-  pabblyWebhook:
-    "https://webhook.zapcloud.com.br/webhook/1e330cfdf8c6e92136c398d6f13351ff",
-};
+// Used for testing / debugging
+// Keep commented if not in use. It won't be minified.
+// const plataforma = {
+//   email: "plataforma@dotconceito.com",
+//   name: "Lorenzo Meneguzzo",
+//   phone: "+5551980109909",
+//   pabblyWebhook:
+//     "https://webhook.zapcloud.com.br/webhook/78d888326a70c8475442792277a0cbc8",
+// };
+
+// Used for testing / debugging
+// Keep commented if not in use. It won't be minified.
+// const gustavo = {
+//   email: "gustavo.j@dotconceito.com",
+//   name: "Gustavo Jesinski",
+//   phone: "+5551998606868", // This is Jordan's Alter Ego Whatsapp instance
+//   pabblyWebhook:
+//     "https://webhook.zapcloud.com.br/webhook/1e330cfdf8c6e92136c398d6f13351ff",
+// };
+
 const lorenzo = {
-  // email: "lorenzo@dotconceito.com",
-  email: "plataforma@dotconceito.com",
+  email: "lorenzo@dotconceito.com",
   name: "Lorenzo Meneguzzo",
   phone: "+5551980109909",
   pabblyWebhook:
@@ -17,5 +29,5 @@ const lorenzo = {
 };
 
 export default function getSalesRepresentatives(): SalesRepresentative[] {
-  return new Array<SalesRepresentative>(gustavo, lorenzo);
+  return new Array<SalesRepresentative>(lorenzo);
 }
